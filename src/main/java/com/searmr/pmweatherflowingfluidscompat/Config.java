39701835a004f8b96c97e40015e6d000a9015373
SearMr-cool <52766691+SearMr-cool@users.noreply.github.com>
2025-07-04 02:01:51 +1200

    Neoforge support for versions down to 147

--- src/main/java/com/searmr/pmweatherflowingfluidscompat/Config.java
+++ src/main/java/com/searmr/pmweatherflowingfluidscompat/Config.java
@@ -15,7 +15,8 @@ import org.jetbrains.annotations.Range;
 // An example config class. This is not required, but it's a good idea to have one to keep your config organized.
 // Demonstrates how to use Neo's config APIs
 @EventBusSubscriber(
-        modid = PmWeatherFlowingFluidsCompat.MODID
+        modid = PmWeatherFlowingFluidsCompat.MODID,
+        bus=EventBusSubscriber.Bus.MOD
 
 )
 public class Config {
