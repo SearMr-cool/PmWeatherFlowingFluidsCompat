58ba55c254a6e5eb4ec71e1500859d8fce5c6335
SearMr-cool <52766691+SearMr-cool@users.noreply.github.com>
2025-07-03 23:13:39 +1200

    Almost 0.1.0a

---
+++ src/main/java/com/searmr/pmweatherflowingfluidscompat/Config.java
@@ -0,0 +1,78 @@
+package com.searmr.pmweatherflowingfluidscompat;
+
+import java.util.List;
+
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.Item;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.fml.event.config.ModConfigEvent;
+import net.neoforged.neoforge.common.ModConfigSpec;
+import org.jetbrains.annotations.Range;
+
+// An example config class. This is not required, but it's a good idea to have one to keep your config organized.
+// Demonstrates how to use Neo's config APIs
+@EventBusSubscriber(
+        modid = PmWeatherFlowingFluidsCompat.MODID
+
+)
+public class Config {
+    private static final ModConfigSpec.Builder BUILDER = new ModConfigSpec.Builder();
+
+
+
+    public static boolean hailDestroysBlocks ;
+    private static final ModConfigSpec.IntValue max_water_amount;
+    public static int maxWaterAmount ;
+    private static final ModConfigSpec.BooleanValue water_drains_rain;
+    public static boolean waterDrainsRain ;
+    private static final ModConfigSpec.BooleanValue is_adaptive;
+    public static boolean isAdaptive ;
+    private static final ModConfigSpec.BooleanValue rain_anywhere;
+    public static boolean rainAnywhere ;
+    private static final ModConfigSpec.IntValue target_tps;
+    public static int targetTps ;
+    private static final ModConfigSpec.IntValue max_drain_chance;
+    public static int maxDrainChance;
+    public static boolean rainFillsBlocks ;
+    private static final ModConfigSpec.BooleanValue rain_fills_blocks;
+    public static final ModConfigSpec thing;
+
+
+
+    @SubscribeEvent
+    private static void onLoad(ModConfigEvent event) {
+        if (!(event instanceof ModConfigEvent.Unloading) && event.getConfig().getSpec() == thing){
+
+            maxWaterAmount = (Integer) max_water_amount.get();
+            waterDrainsRain = (boolean) water_drains_rain.get();
+            isAdaptive = (boolean) is_adaptive.get();
+            rainAnywhere = (boolean) rain_anywhere.get();
+            targetTps = (int) target_tps.get();
+            maxDrainChance = (int) max_drain_chance.get();
+            rainFillsBlocks = (boolean) rain_fills_blocks.get();
+        }
+
+
+    }
+
+
+
+    static  {
+
+        max_water_amount = BUILDER.comment("The max amount of water that can be added to water during rain (This is the biggest factor when it comes to how fast flooding happens").defineInRange("maxwateramount",4,1,100);
+        water_drains_rain = BUILDER.comment("Should rain be drained while it is raining? If enabled it will make flooding very difficult plus lag may occur so it is recommended to leave this off").define("waterdrainsrain",false);
+        is_adaptive = BUILDER.comment("If enabled the amount of rain will dynamically adjust to try and stop overloading the users system (do note the highest this will go is still limited by the user set value").define("isadaptive",true);
+        target_tps = BUILDER.comment("If adaptive is enabled each tick will aim to be this many milliseconds long").defineInRange("targettps",30,1,50);
+        rain_anywhere = BUILDER.comment("By default Flowing Fluids does not allow rain to build up in oceans/rivers (and some other biomes), this setting if enabled will bypass this default behavior making flooding easier but it may cause more lag in the long run").define("rainanywhere",true);
+        max_drain_chance = BUILDER.comment("The maximum drain chance for water to be drained (This is required as some values here are changed dynamically (0-100%)").defineInRange("maxdrainchance",6,0,100);
+        rain_fills_blocks = BUILDER.comment("If rain places blocks above water (This must be turned on for flooding to properly occur)").define("rainfillsblocks",true);
+
+
+
+        thing = BUILDER.build();
+    }
+}
+
---
+++ src/main/java/com/searmr/pmweatherflowingfluidscompat/mixin/MixinWaterFluid.java
@@ -0,0 +1,193 @@
+package com.searmr.pmweatherflowingfluidscompat.mixin;
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
+
+
+import com.searmr.pmweatherflowingfluidscompat.Config;
+import com.searmr.pmweatherflowingfluidscompat.FlowingFluidsCompat;
+import com.searmr.pmweatherflowingfluidscompat.PmWeatherFlowingFluidsCompat;
+import dev.protomanly.pmweather.event.GameBusEvents;
+import dev.protomanly.pmweather.weather.WeatherHandler;
+import it.unimi.dsi.fastutil.Pair;
+import net.minecraft.core.BlockPos;
+import net.minecraft.tags.BiomeTags;
+import net.minecraft.tags.FluidTags;
+import net.minecraft.util.Mth;
+import net.minecraft.util.RandomSource;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.LevelReader;
+import net.minecraft.world.level.LightLayer;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.material.FlowingFluid;
+import net.minecraft.world.level.material.Fluid;
+import net.minecraft.world.level.material.FluidState;
+import net.minecraft.world.level.material.WaterFluid;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import traben.flowing_fluids.FFFluidUtils;
+import traben.flowing_fluids.FlowingFluids;
+
+
+@Mixin({WaterFluid.class})
+public abstract class MixinWaterFluid extends FlowingFluid {
+    @Unique
+    boolean isWithinInfBiomeHeights = false;
+    @Unique
+    boolean isInfBiome = false;
+    @Unique
+    boolean hasSkyLight = false;
+
+    @Shadow
+    public abstract int getDropOff(LevelReader var1);
+
+    @Shadow
+    public abstract boolean isSame(Fluid var1);
+
+    protected void randomTick(Level level, BlockPos blockPos, FluidState fluidState, RandomSource randomSource) {
+        super.randomTick(level, blockPos, fluidState, randomSource);
+
+        if (!level.isClientSide() && !fluidState.isEmpty() && FlowingFluids.config.enableMod && FlowingFluids.config.isFluidAllowed(fluidState)) {
+            if (!FlowingFluids.config.dontTickAtLocation(blockPos, level)) {
+
+                var managers = GameBusEvents.MANAGERS;
+                WeatherHandler handle = (WeatherHandler) managers.get(level.dimension());
+                float rainLevel = handle.getPrecipitation(blockPos.getCenter());
+                boolean isRaining = rainLevel > 0;
+                this.isWithinInfBiomeHeights = FlowingFluids.config.fastBiomeRefillAtSeaLevelOnly ? level.getSeaLevel() == blockPos.getY() || level.getSeaLevel() - 1 == blockPos.getY() : level.getSeaLevel() == blockPos.getY() && blockPos.getY() > 0;
+                this.hasSkyLight = level.getBrightness(LightLayer.SKY, blockPos) > 0;
+                this.isInfBiome = FFFluidUtils.matchInfiniteBiomes(level.getBiome(blockPos));
+                int amount = fluidState.getAmount();
+                if (amount < 8) {
+                    if (this.ff$tryBiomeFillOrDrain(level, blockPos, amount, level.random.nextFloat(),isRaining)) {
+                        if (FlowingFluids.config.printRandomTicks) {
+                            String var8 = String.valueOf(blockPos);
+                            FlowingFluids.info("--- Water was filled by biome at " + var8 + ". Chance: " + FlowingFluids.config.oceanRiverSwampRefillChance);
+                        }
+
+                        return;
+                    }
+
+                    if (this.ff$tryRainFill(level, blockPos, level.random.nextFloat(),isRaining,rainLevel)) {
+                        if (FlowingFluids.config.printRandomTicks) {
+                            String var7 = String.valueOf(blockPos);
+                            FlowingFluids.info("--- Water was filled by rain at " + var7 + ". Chance: " + FlowingFluids.config.rainRefillChance);
+                        }
+
+                        return;
+                    }
+
+                    if (this.ff$tryEvaporateNether(level, blockPos, amount, level.random.nextFloat())) {
+                        if (FlowingFluids.config.printRandomTicks) {
+                            String var6 = String.valueOf(blockPos);
+                            FlowingFluids.info("--- Water was evaporated via Nether at " + var6 + ". Chance: " + FlowingFluids.config.evaporationChanceV2);
+                        }
+
+                        return;
+                    }
+
+                    if (this.ff$tryEvaporate(level, blockPos, amount, level.random.nextFloat(),isRaining) && FlowingFluids.config.printRandomTicks) {
+                        String var10000 = String.valueOf(blockPos);
+                        FlowingFluids.info("--- Water was evaporated - non Nether at " + var10000 + ". Chance: " + FlowingFluids.config.evaporationChanceV2);
+                    }
+                } else if (this.ff$tryRainFill(level, blockPos, level.random.nextFloat(),isRaining,rainLevel) && FlowingFluids.config.printRandomTicks) {
+                    String var9 = String.valueOf(blockPos);
+                    FlowingFluids.info("--- Water was filled by rain at " + var9 + ". Chance: " + FlowingFluids.config.rainRefillChance);
+                }
+
+            }
+        }
+    }
+
+    @Unique
+    private boolean ff$tryRainFill(Level level, BlockPos blockPos, float chance, boolean isRaining,float rainLevel) {
+        if (chance < Math.min(FlowingFluids.config.rainRefillChance, FlowingFluids.config.evaporationChanceV2 / 3.0F) && isRaining && level.canSeeSky(blockPos.above()) && (!this.isInfBiome || !this.isWithinInfBiomeHeights || Config.rainAnywhere) && !level.getBiome(blockPos).is(BiomeTags.HAS_VILLAGE_DESERT)) {
+            int amount = Math.clamp((int)(FlowingFluidsCompat.maxRainAmount * rainLevel),0, Config.maxWaterAmount);
+            if (Config.isAdaptive) FlowingFluidsCompat.tempRainArray.add(true);
+            Pair<Integer, Runnable> result = FFFluidUtils.placeConnectedFluidAmountAndPlaceAction(level, blockPos, amount, this, 40, FlowingFluids.config.rainFillsWaterHigherV2, false);
+            if ((Integer)result.first() != amount) {
+
+                ((Runnable)result.second()).run();
+
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Unique
+    private boolean ff$tryBiomeFillOrDrain(Level level, BlockPos blockPos, int amount, float chance, boolean isRaining) {
+        if (!Config.waterDrainsRain && isRaining) return false;
+        if (level.getSeaLevel() == blockPos.getY()) {
+            if (chance < FlowingFluids.config.infiniteWaterBiomeNonConsumeChance || chance < FlowingFluids.config.oceanRiverSwampRefillChance || isRaining && chance < FlowingFluids.config.rainRefillChance) {
+                FluidState below = level.getFluidState(blockPos.below());
+                if (below.getAmount() == 8 && below.is(FluidTags.WATER) && this.hasSkyLight && this.isInfBiome) {
+                    level.setBlockAndUpdate(blockPos, FFFluidUtils.getBlockForFluidByAmount(this, amount - 2));
+                    return true;
+                }
+            }
+        } else if (this.isWithinInfBiomeHeights && amount < 8 && chance < FlowingFluids.config.oceanRiverSwampRefillChance && this.isInfBiome && this.hasSkyLight) {
+            level.setBlockAndUpdate(blockPos, FFFluidUtils.getBlockForFluidByAmount(this, amount + 2));
+            return true;
+        }
+
+        return false;
+    }
+
+    @Unique
+    private boolean ff$tryEvaporate(Level level, BlockPos blockPos, int amount, float chance,boolean isRaining) {
+        if (!Config.waterDrainsRain && isRaining) return false;
+        if (chance < FlowingFluids.config.evaporationChanceV2 && amount <= this.getDropOff(level) && level.getFluidState(blockPos.below()).isEmpty()) {
+            level.setBlockAndUpdate(blockPos, Blocks.AIR.defaultBlockState());
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Unique
+    private boolean ff$tryEvaporateNether(Level level, BlockPos blockPos, int amount, float chance) {
+        if (chance < FlowingFluids.config.evaporationNetherChance && level.getBiome(blockPos).is(BiomeTags.IS_NETHER)) {
+            if (amount == 1) {
+                level.setBlockAndUpdate(blockPos, Blocks.AIR.defaultBlockState());
+            } else {
+                level.setBlockAndUpdate(blockPos, FFFluidUtils.getBlockForFluidByAmount(this, amount - 3));
+            }
+
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Inject(
+            method = {"getSlopeFindDistance(Lnet/minecraft/world/level/LevelReader;)I"},
+            at = {@At("RETURN")},
+            cancellable = true
+    )
+    private void ff$modifySlopeDistance(LevelReader level, CallbackInfoReturnable<Integer> cir) {
+        if (FlowingFluids.config.enableMod && FlowingFluids.config.isFluidAllowed(this)) {
+            cir.setReturnValue(Mth.clamp(FlowingFluids.config.waterFlowDistance, 1, 8));
+        }
+
+    }
+
+    @Inject(
+            method = {"getTickDelay(Lnet/minecraft/world/level/LevelReader;)I"},
+            at = {@At("RETURN")},
+            cancellable = true
+    )
+    private void ff$modifyTickDelay(LevelReader level, CallbackInfoReturnable<Integer> cir) {
+        if (FlowingFluids.config.enableMod && FlowingFluids.config.isFluidAllowed(this)) {
+            cir.setReturnValue(Mth.clamp(FlowingFluids.config.waterTickDelay, 1, 255));
+        }
+
+    }
+}
+
